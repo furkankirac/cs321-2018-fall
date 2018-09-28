25795af4ad00b3122b16f898b51f3c4da74abb34
Furkan Kıraç <fkirac@gmail.com>
2018-09-28 12:14:41 +0300

    Stupid C function usage.

--- main.cpp
+++ main.cpp
@@ -1,10 +1,31 @@
 #include "stdio.h"
+#include "string.h"
+
+struct Data
+{
+    char name[100];
+    int age;
+};
+
+void printTitle()
+{
+    int k = 0;
+    Data d;
+//    d.name[0] = 'A';
+//    d.name[1] = 0;
+
+    strcpy(d.name, "HELLO");
+
+    printf("This is our first CS321 program :)\n");
+    printf("[%s]\n", d.name);
+}
+
 
 int main(int argc, char* argv[])
 {
+    printTitle();
     printf("Number of arguments passed: %d\n", argc);
     for(int i=0; i<argc; ++i)
-    {
-        printf("'%s'\n", argv[i]);
-    }
+        printf("'%lx'\n", argv[i]);
+
 }
