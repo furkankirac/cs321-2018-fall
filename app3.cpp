37923c497243ec5f20565c991454da7286ce594c
Furkan Kıraç <fkirac@gmail.com>
2018-10-23 09:15:54 +0300

    Importance of using const in references.

--- app3.cpp
+++ app3.cpp
@@ -5,26 +5,25 @@
 
 #include <iostream>
 
-using functype = void(*)();
+using functype = float(*)(const int&);
 
-void doIt()
+float doIt(const int& some_value)
 {
-    std::cout << "doIt()\n";
-}
-
-void justDoIt()
-{
-    std::cout << "justDoIt()\n";
+    std::cout << "doIt(" << some_value << ")\n";
+    return some_value + 0.1f;
 }
 
 void runner(functype func_to_call)
 {
     for(int i=0; i<10; ++i)
-        func_to_call();
+    {
+        float retval = func_to_call(100);
+        std::cout << "returned value = " << retval << "\n";
+    }
 }
 
 int main(int argc, char* argv[])
 {
 //    functype a = &doIt;
-    runner(&justDoIt);
+    runner(&doIt);
 }
