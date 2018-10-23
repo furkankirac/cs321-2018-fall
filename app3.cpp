04fed133960b0457e424186debf32b398abd715d
Furkan Kıraç <fkirac@gmail.com>
2018-10-23 10:34:23 +0300

    Lambda functions.

--- app3.cpp
+++ app3.cpp
@@ -24,20 +24,26 @@ public:
     }
 };
 
-class Do10Times
-{
-public:
-
-    void operator() (const WriteYourself& obj)
-    {
-        for(int i=0; i<10; ++i)
-            obj();
-    }
-
-};
-
 int main(int argc, char* argv[])
 {
-    Do10Times do_ten_times;
-    do_ten_times(WriteYourself(100));
+    auto v1 = 10;
+    auto v2 = 100.5f;
+    auto v3 = 100.5;
+    auto v4 = "hello";
+
+    auto numCalls = 0;
+    auto times = 5;
+
+    auto do_10_times = [&numCalls, times]() {
+        ++numCalls;
+        for(int i=0; i<times; ++i)
+        {
+            auto a = WriteYourself(i);
+            a();
+        }
+
+    };
+
+    do_10_times();
+    std::cout << "NC = " << numCalls << "\n";
 }
