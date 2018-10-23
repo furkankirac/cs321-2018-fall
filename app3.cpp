164d25c95533afad0ce1ec8fde7b14d28f854604
Furkan Kıraç <fkirac@gmail.com>
2018-10-23 09:03:20 +0300

    Basic function pointers.

--- app3.cpp
+++ app3.cpp
@@ -1,7 +1,30 @@
-//functions, function pointers, function objects, lambdas
+//functions - C
+//function pointers - C
+//function objects - C++ (old)
+//lambdas - C++11
 
 #include <iostream>
 
+using functype = void(*)();
+
+void doIt()
+{
+    std::cout << "doIt()\n";
+}
+
+void justDoIt()
+{
+    std::cout << "justDoIt()\n";
+}
+
+void runner(functype func_to_call)
+{
+    for(int i=0; i<10; ++i)
+        func_to_call();
+}
+
 int main(int argc, char* argv[])
 {
+//    functype a = &doIt;
+    runner(&justDoIt);
 }
