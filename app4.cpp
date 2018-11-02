1db2537bbc20443be76d1c5576e54185c7ebf888
Furkan Kıraç <fkirac@gmail.com>
2018-11-02 12:33:12 +0300

    Template typename

--- app4.cpp
+++ app4.cpp
@@ -15,12 +15,12 @@ struct Accumulate
 
 
 // instead of below manual coding
-//auto getAccumulate_10_20() { return Accumulate<30>(); }
-//auto getAccumulate_100_1() { return Accumulate<101>(); }
-
+//auto getAccumulate_int_10_20() { return Accumulate<30>(); }
+//auto getAccumulate_int_100_1() { return Accumulate<101>(); }
+//auto getAccumulate_unsignedshort_1000_11() { return Accumulate<1011>(); }
 
 // do this instead
-template<int a, int b>
+template<typename T, T a, T b>
 auto getAccumulate() // function template
 {
     return Accumulate<a+b>();
@@ -31,13 +31,15 @@ int main(int argc, char* argv[])
 {
     const int v = 200;
 
-    auto c = getAccumulate<10, 20>(); // getAccumulate_10_20();
-    auto d = getAccumulate<100, 1>();
-    auto e = getAccumulate<v, 1>();
+    auto c = getAccumulate<int, 10, 20>(); // getAccumulate_10_20();
+    auto d = getAccumulate<int, 100, 1>();
+    auto e = getAccumulate<int, v, 1>();
+    auto f = getAccumulate<unsigned short, 1000, 11>();
 
     c(); // 30
     d(); // 101
     e(); // 201
+    f(); // 1011
 
 //    std::cout << 30 << std::endl;
 //    std::cout << 101 << std::endl;
