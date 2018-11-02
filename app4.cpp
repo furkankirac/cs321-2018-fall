a819a82b8da8964058910e1431f683a1eb747818
Furkan Kıraç <fkirac@gmail.com>
2018-11-02 10:47:20 +0300

    Lecture start.

---
+++ app4.cpp
@@ -0,0 +1,11 @@
+// Accumulate class. operator << overload example. also as free function.
+// Int operator = (const Int& other){ return other;} /// nooooo
+// compile-time vs run-time
+// templates: generic programming
+
+#include <iostream>
+
+int main(int argc, char* argv[])
+{
+    return 0;
+}
