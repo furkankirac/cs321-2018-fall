34b96dca115a3ab7ae169868e37e0df46d1cdd0d
Furkan Kıraç <fkirac@gmail.com>
2018-11-02 11:56:25 +0300

    operator overloading as free functions

--- app4.cpp
+++ app4.cpp
@@ -1,45 +1,36 @@
 // Accumulate class. operator << overload example. also as free function.
-// Int operator = (const Int& other){ return other;} /// nooooo
 // compile-time vs run-time
 // templates: generic programming
 
 #include <iostream>
 
-struct Int
+struct Accumulate
 {
     int value;
 
-    Int(int value = 0) : value(value) // ctor #1
-    {
-        std::cout << "ctor #1" << std::endl;
-    }
-
-    Int(const Int& other) // ctor #2
-    {
-        value = other.value;
-        std::cout << "ctor #2" << std::endl;
-    }
-
-    Int& operator =(const Int& other) // copy-assignment operator
-    {
-        value = other.value;
-        std::cout << "copy-assignment" << std::endl;
-
-        return *this;
-    }
+    Accumulate(int value = 0) : value(value) { }
 };
+
+Accumulate& operator <<(Accumulate& a, int v) // func #1
+{
+    std::cout << "BBBB";
+    a.value += v;
+    return a;
+}
+
+Accumulate& operator <<(Accumulate& a, const char*) // func #2
+{
+    std::cout << "AAAAAA";
+    ++a.value;
+    return a;
+}
+
 
 int main(int argc, char* argv[])
 {
-    Int v1(10); // ctor #1
-    Int v2(v1); // ctor #2
-    Int v3(v2); // ctor #2 again
-    Int v4;
-    v4 = v3 = v2; // copy-assignment
+    Accumulate a;
+    a << 10 << 20 << "hi guys"; // "....." this is const char*
 
-    std::cout << v1.value << std::endl;
-    std::cout << v2.value << std::endl;
-    std::cout << v3.value << std::endl;
-
+    std::cout << a.value << std::endl;
     return 0;
 }
