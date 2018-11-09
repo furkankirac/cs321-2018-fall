4f74bf0138316329d891b1c8195b0674d5ea6222
Furkan Kıraç <fkirac@gmail.com>
2018-11-09 12:27:47 +0300

    Copy constructors, converting constructors, copy assigment are template based.

--- app5.cpp
+++ app5.cpp
@@ -2,23 +2,49 @@
 
 #include <iostream>
 
-template<typename T>
+template<typename T, int N>
 class ArrayList
 {
-    T values[10] = {0};
+    T values[N] = {0};
 
 public:
-//    ArrayList() = default;
+    ArrayList() = default;
+    inline T& operator[](int index)
+    { return values[index]; }
+
     inline T operator[](int index) const
     { return values[index]; }
+
+    template<typename U>
+    ArrayList(const ArrayList<U, N>& other)
+    {
+        for(int i=0; i<N; ++i)
+            values[i] = other[i];
+    }
+
+    template<typename U>
+    ArrayList<T, N>& operator =(const ArrayList<U, N>& other)
+    {
+        for(int i=0; i<N; ++i)
+            values[i] = other[i];
+        return *this;
+    }
+
 
 };
 
 int main(int argc, char* argv[])
 {
-    int a = int();
+    ArrayList<short, 10> S;
+    ArrayList<char, 10> C;
+    ArrayList<int, 10> L = S;
+    ArrayList<int, 10> L2 = C;
 
-    ArrayList<int> L;
+//    ArrayList<ArrayList<int>> A;
+
+
+
+    L[0] = 10;
 
     return L[0];
 }
