52df0ae1b88ef3046c6dfee405c8a6d759ae61a1
Furkan Kıraç <fkirac@gmail.com>
2018-11-09 08:10:29 +0300

    Lecture start.

---
+++ app5.cpp
@@ -0,0 +1,12 @@
+// operator precedence: chaining =, chaining other things
+// inline functions
+// specialization of function templates
+// factorial evaluated at compile time using template meta programming
+// typename vs class. type and non-type template parameters
+
+#include <iostream>
+
+int main(int argc, char* argv[])
+{
+    return 0;
+}
