50eb9b9ac43c2c460abcd5ab62a6aae587c5fe78
Furkan Kıraç <fkirac@gmail.com>
2018-11-16 11:30:36 +0300

    OlderThan40, OlderThanX, function objects, lambdas.

--- app6.cpp
+++ app6.cpp
@@ -1,6 +1,94 @@
+// value vs reference semantics
+// OOP includes encapsulation and inheritence
+// C++ STL is example of Generics + FunctionalProgramming
+// std::vector<T>
+// OlderThan40: range-for, Function Object, Lambda
+// OlderThanX: Function Object, Lambda
+// returning auto
+// forEach<FuncType>, forEach<T, FuncType>
+
 #include <iostream>
+#include <vector>
 
 int main(int argc, char* argv[])
 {
+    std::vector<int> person_ages = { 10, 20, 3, 42, 50 };
+
+    auto sz = person_ages.size();
+    std::cout << "Num of ints in vector: " << sz << std::endl;
+
+    // old way
+    {
+        std::cout << "Old Way\n";
+        for(int i=0; i<sz; ++i)
+        {
+            auto v = person_ages[i];
+            if(v > 40)
+                std::cout << v << std::endl;
+        }
+    }
+
+    { // slightly better way
+        std::cout << "Range For\n";
+        for(int v : person_ages)
+        {
+            if(v > 40)
+                std::cout << v << std::endl;
+        }
+    }
+
+    { // function object
+        struct OlderThan40
+        {
+            bool operator()(int age) const { return age > 40; }
+        };
+
+        std::cout << "OlderThan40 function object\n";
+        OlderThan40 olderThan40;
+        for(int v : person_ages)
+        {
+            if(olderThan40(v))
+                std::cout << v << std::endl;
+        }
+    }
+
+    {
+        std::cout << "OlderThan40 lambda\n";
+        auto olderThan40 = [](int age) -> bool { return age > 40; };
+        for(int v : person_ages)
+        {
+            if(olderThan40(v))
+                std::cout << v << std::endl;
+        }
+    }
+
+    {
+        struct OlderThanX
+        {
+            int x;
+            OlderThanX(int x) : x(x) { }
+            bool operator()(int age) const { return age > x; }
+        };
+        std::cout << "OlderThanX initialized with 40 (function object)\n";
+        int x = 40;
+        OlderThanX olderThanX(x);
+        for(int v : person_ages)
+        {
+            if(olderThanX(v))
+                std::cout << v << std::endl;
+        }
+    }
+
+    {
+        std::cout << "OlderThanX initialized with 40 (lambda)\n";
+        int x = 40;
+        auto olderThanX = [=](int age) { return age > x; };
+        for(int v : person_ages)
+        {
+            if(olderThanX(v))
+                std::cout << v << std::endl;
+        }
+    }
+
     return 0;
 }
