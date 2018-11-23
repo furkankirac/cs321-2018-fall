ac17102ca7db1b05580b47741d61020b1470d425
Furkan Kıraç <fkirac@gmail.com>
2018-11-23 11:14:57 +0300

    Multiplication in terms of pure functional programming in C++ (version 1). This doesn't work. Functions cannot be partially specialized due to overload resolution problems.

--- app7.cpp
+++ app7.cpp
@@ -1,8 +1,10 @@
 // pure functions vs functions with state (doing assignments to variables)
 // template<int> factorial example is pure
-// Pure functional languages have no mutable data. Therefore, they use recursive definitions instead of loops.
+// Pure functional languages have no mutable data.
+// Therefore, they use recursive definitions instead of loops.
 // from the compiler's point of view:
-// template<int> factorial example is not recursive. Only the definition looks recursive
+// template<int> factorial example is not recursive.
+// Only the definition looks recursive
 
 // example: haskell example for definition of multiplication in terms of addition
 // mult n 0 = 0
@@ -11,7 +13,28 @@
 
 #include <iostream>
 
+template<int n, int m>
+int mult()
+{
+    return mult<n, m-1>() + n;
+}
+
+template<int n>
+int mult<n, 1>()
+{
+    return n;
+}
+
+template<int n>
+int mult<n, 0>()
+{
+    return 0;
+}
+
 int main(int argc, char* argv[])
 {
+    using namespace std;
+
+    cout << mult<10, 5>() << endl;
     return 0;
 }
