9d768f867735963a61145fa26f3160868c72b899
Furkan Kıraç <fkirac@gmail.com>
2018-11-23 11:55:10 +0300

    Use a vector. Go over it in order by using state variable i. Calculate squares, put them in another container. old style.

--- app8.cpp
+++ app8.cpp
@@ -10,7 +10,7 @@
 // Google's MapReduce?
 
 #include <iostream>
-//#include <vector>
+#include <vector>
 //#include <list>
 //#include <algorithm>
 //#include <numeric>
@@ -18,6 +18,20 @@
 int main(int argc, char* argv[])
 {
     using namespace std;
+
+    vector<int> v = { 1, 2, 3, 4, 5 };
+    vector<int> y(v.size());
+
+    for(int i=0; i<v.size(); ++i)
+        y[i] = v.at(i) * v.at(i);
+
+    for(auto val : v)
+        cout << val << ", ";
+    cout << endl;
+
+    for(auto val : y)
+        cout << val << ", ";
+    cout << endl;
 
     return 0;
 }
