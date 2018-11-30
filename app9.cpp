78c1ef19a8d0f0d0d099bd65239e3c850639af87
Furkan Kıraç <fkirac@gmail.com>
2018-11-30 12:08:16 +0300

    Focus on move construction and assignment.

--- app9.cpp
+++ app9.cpp
@@ -20,29 +20,50 @@ using namespace std;
 
 struct Point
 {
-    int x;
-    int y;
+    int x{};
+    int y{};
+    string name{};
 
     Point(int x, int y) : x(x), y(y)
     {
         cout << "ctor\n";
     }
 
-    Point(std::initializer_list<int> il)
+    Point(std::initializer_list<int> il) :
+        x{*il.begin()},
+        y{*(il.begin()+1)}
     {
         cout << "ctor with init-list\n";
     }
 
-    Point(const Point& other) : x(other.x), y(other.y)
+    Point(const Point& other) : x(other.x), y(other.y), name(other.name)
     {
         cout << "copy ctor\n";
+    }
+
+    Point(Point&& other) :
+        x(other.x),
+        y(other.y),
+        name(move(other.name))
+    {
+        cout << "move ctor\n";
     }
 
     Point& operator =(const Point& other)
     {
         x = other.x;
         y = other.y;
+        name = other.name;
         cout << "copy assignment\n";
+        return *this;
+    }
+
+    Point& operator =(Point&& other)
+    {
+        x = other.x;
+        y = other.y;
+        name = (string&&)other.name;
+        cout << "move assignment\n";
         return *this;
     }
 
@@ -61,42 +82,44 @@ int main(int argc, char* argv[])
 
     Point p1{10, 20};
     cout << endl;
-//    cout << "x=" << p1.x << endl;
-//    cout << "y=" << p1.y << endl;
+////    cout << "x=" << p1.x << endl;
+////    cout << "y=" << p1.y << endl;
 
-    Point p2 = p1; // copy ctor
-    cout << endl;
+//    Point p2 = p1; // copy ctor
+//    cout << endl;
 
-    Point p3(p1); // copy ctor
-    cout << endl;
+//    Point p3(p1); // copy ctor
+//    cout << endl;
 
-    Point p4{p1}; // copy ctor
-    cout << endl;
+//    Point p4{p1}; // copy ctor
+//    cout << endl;
 
-    p1 = p2; // copy assignment
-    cout << endl;
+//    p1 = p2; // copy assignment
+//    cout << endl;
 
     p1 = {50, 50}; // ctor with init-list + copy assignment
     cout << endl;
 
-    Point p5 = {50, 50};
-    cout << endl;
+    Point pp = move(p1);
 
-    map<int, string> m{
-        {10, "ten"},
-        {20, "twenty"}
-    };
+//    Point p5 = {50, 50};
+//    cout << endl;
 
-    m[10] = "TEN";
-    try {
-        cout << m.at(25) << endl;
-    } catch(...)
-    {
-        cout << "exception\n";
-    }
+//    map<int, string> m{
+//        {10, "ten"},
+//        {20, "twenty"}
+//    };
 
-//    int i = 2.0; // this will work
-//    int i{2.0}; // this won't compile, which is good
+//    m[10] = "TEN";
+//    try {
+//        cout << m.at(25) << endl;
+//    } catch(...)
+//    {
+//        cout << "exception\n";
+//    }
+
+////    int i = 2.0; // this will work
+////    int i{2.0}; // this won't compile, which is good
 
     return 0;
 }
