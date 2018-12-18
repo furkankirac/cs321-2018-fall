d9f78b5b6c8cbd6a7f4960d3acc9c223776ae324
Furkan Kıraç <fkirac@gmail.com>
2018-12-18 09:19:00 +0300

    Function composition using templates and generic lambdas.

--- app11.cpp
+++ app11.cpp
@@ -21,8 +21,37 @@ using namespace std;
 // structured binding
 // ranges library quick reference
 
+template<typename F, typename G>
+auto compose_(F f, G g)
+{
+    return [=](auto x) {
+        return f(g(x));
+    };
+}
+
+auto compose = [](auto f, auto g) {
+    return [=](auto x) {
+        return f(g(x));
+    };
+};
+
+double fahrenheitToCelsius(double fahrenheit)
+{
+    return (fahrenheit - 32) / 180.0 * 100.0;
+}
+
+bool isRoomTemperature(double celcius)
+{
+    return celcius >= 20.0 && celcius <= 25.0;
+}
+
+
 int main(int argc, char* argv[])
 {
+    auto func = compose(isRoomTemperature, fahrenheitToCelsius);
+    bool isRoomTemp = func(70);
+
+    cout << isRoomTemp << endl;
 
     return 0;
 }
